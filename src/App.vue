90d14a514e76dfc8a73e4cd3c2cf63873f794882
coder-lcn <changnan666@gmail.com>
2022-11-08 03:20:06 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, nextTick, onMounted, ref, watch } from 'vue';
 import list from './article.json'
 
 const edit = ref(false);
 const item = ref(null);
 const value = ref("");
+const textareaRef = ref(null)
+const error = ref({ wrong: false, source: 'the young man said rudel', yours: 'the young man said rudel' })
 
 const scroll = e => {
   edit.value && e.preventDefault();
@@ -17,26 +19,61 @@ const onEdit = selectItem => {
   edit.value = true;
 }
 
-const formatContent = target => target.split('.').map(item => item.trim(''))
+const formatContent = target => target.replace(/(\'|\!|")/g, '').split('.').map(item => item.trim(''))
 
 const onDone = () => {
   const source = formatContent(item.value.content);
   const target = formatContent(value.value);
 
   for (let i = 0; i < source.length; i++) {
-    if (source[i] !== target[i]) {
-      alert(`
-      ERROR ❌
-      
-        source: ${source[i]}
-         yours: ${target[i]}
-      `);
-
+    if (source[i].trim() !== target[i].trim()) {
+      error.value = {
+        wrong: true,
+        source: source[i],
+        yours: target[i]
+      };
+      console.warn(source[i], target[i]);
+      textareaRef.value.blur();
       break;
     } else if (i === source.length - 1) {
       alert('🎉 Congratulations!');
     }
   }
+}
+
+const onTransitionend = () => {
+  value.value = '';
+}
+
+watch(edit, (val) => {
+  if (val) {
+    nextTick(() => {
+      textareaRef.value.focus();
+    })
+  }
+});
+
+onMounted(() => {
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && error.value.wrong === false) {
+      edit.value = false;
+    }
+
+
+    if (error.value.wrong) {
+      e.preventDefault();
+      error.value.wrong = false;
+
+      if (textareaRef.value) {
+        textareaRef.value.value = textareaRef.value.value.trim();
+        textareaRef.value.focus();
+      }
+    }
+  })
+})
+
+const onEnter = () => {
+  if (edit.value) onDone();
 }
 </script>
 
@@ -44,15 +81,30 @@ const onDone = () => {
   <ul @touchmove="scroll" @wheel="scroll">
     <li v-for="(item, i) in list" @click="onEdit(item)">
       Lesson {{ i + 1 }}&nbsp;&nbsp;&nbsp;&nbsp;{{ item.title }}</li>
-    <div class="editor">
+    <div class="editor" @transitionend="onTransitionend">
       <div class="action">
         <span @click="edit = false">🔙</span>
         <div v-if="item">{{ item.title }}</div>
         <span class="done" @click="onDone">DONE</span>
       </div>
-      <textarea v-model="value" @touchmove.stop @wheel.stop />
+      <textarea ref="textareaRef" v-model="value" @touchmove.stop @wheel.stop @keydown.stop.enter="onEnter" />
     </div>
   </ul>
+  <div tabindex="0" class="dialog" :class="{ active: error.wrong }" @touchstart="error.wrong = false"
+    @keydown.enter="error.wrong = false">
+    <table border>
+      <tbody>
+        <tr class="wrong-row">
+          <td>Yours</td>
+          <td>{{ error.yours }}</td>
+        </tr>
+        <tr>
+          <td>Source</td>
+          <td>{{ error.source }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style>
@@ -139,11 +191,48 @@ li {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  user-select: none;
 }
 
 .editor .done {
   right: 0;
   font-size: 18px;
   font-weight: bold;
+}
+
+.dialog {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(0, 0, 0, 0.8);
+  display: none;
+  justify-content: center;
+  align-items: center;
+}
+
+.dialog.active {
+  display: flex;
+}
+
+th,
+td {
+  border: none;
+}
+
+.dialog table {
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #fff;
+  border-spacing: 0px;
+}
+
+td {
+  padding: 10px;
+}
+
+.wrong-row {
+  color: red;
 }
 </style>
