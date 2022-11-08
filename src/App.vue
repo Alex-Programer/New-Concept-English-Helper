3baa6507597b9bad65050fab3f389e6c1b768d12
Alex-Programer <alex.lichangnan@gmail.com>
2022-11-08 16:41:45 +0800

    update regex

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ const onEdit = selectItem => {
   edit.value = true;
 }
 
-const formatContent = target => target.replace(/(|\'|\!|")/g, '').replace(/\s+/g, ' ').split('.').map(item => item.trim(''))
+const formatContent = target => target.replace(/('|\!|")/g, '').replace(/\s+/g, ' ').split('.').map(item => item.trim(''))
 
 const onDone = () => {
   const source = formatContent(item.value.content);
@@ -38,7 +38,6 @@ const onDone = () => {
       break;
     } else if (i === source.length - 1) {
       alert('🎉 Congratulations!');
-      value.value = '';
     }
   }
 }
